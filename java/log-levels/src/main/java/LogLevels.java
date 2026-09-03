c9a23ccd52c41010a6077bdaf0c0943701b3ba8d
Manu <manusolerj@gmail.com>
2026-09-03 19:32:09 +0200

    Feat: Completed Log Levels exercise

---
+++ java/log-levels/src/main/java/LogLevels.java
@@ -0,0 +1,17 @@
+public class LogLevels {
+    
+    public static String message(String logLine) {
+        return logLine.substring(logLine.indexOf(":") + 1).trim();
+    }
+
+    public static String logLevel(String logLine) {
+        return logLine.substring(logLine.indexOf("[") + 1, logLine.indexOf("]")).toLowerCase();
+    }
+
+    public static String reformat(String logLine) {
+        String message = message(logLine);
+        String log = "(" + logLevel(logLine) + ")";
+
+        return message + " " + log;
+    }
+}
